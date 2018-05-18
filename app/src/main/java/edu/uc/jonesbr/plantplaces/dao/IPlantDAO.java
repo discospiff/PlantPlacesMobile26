f090edf333a0ba8da7e51a97eda6fe585a31fcca
jonesbr <brandan.jones@uc.edu>
2018-05-17 20:32:14 -0400

    Start DTO and DAO interface.

---
+++ app/src/main/java/edu/uc/jonesbr/plantplaces/dao/IPlantDAO.java
@@ -0,0 +1,20 @@
+package edu.uc.jonesbr.plantplaces.dao;
+
+import java.util.List;
+
+import edu.uc.jonesbr.plantplaces.dto.PlantDTO;
+
+/**
+ *  Represent the data access operations for a plant.
+ *  Created by ucint on 5/17/2018.
+ */
+
+public interface IPlantDAO {
+
+    /**
+     * Given the search term, find appropriate matching plants.
+     * @param searchTerm A text representation of the user's search term.
+     * @return a list of matching plants.
+     */
+    public List<PlantDTO> search(String searchTerm);
+}
---
+++ app/src/main/java/edu/uc/jonesbr/plantplaces/dto/PlantDTO.java
@@ -0,0 +1,64 @@
+package edu.uc.jonesbr.plantplaces.dto;
+
+/**
+ * A plant has a genus, species, cultivar, common name.
+ * Created by ucint on 5/17/2018.
+ */
+public class PlantDTO {
+
+    private int guid;
+    private int cacheId;
+    private String genus;
+    private String species;
+    private String cultivar;
+    private String common;
+
+
+    public int getGuid() {
+        return guid;
+    }
+
+    public void setGuid(int guid) {
+        this.guid = guid;
+    }
+
+    public String getGenus() {
+        return genus;
+    }
+
+    public void setGenus(String genus) {
+        this.genus = genus;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public String getCultivar() {
+        return cultivar;
+    }
+
+    public void setCultivar(String cultivar) {
+        this.cultivar = cultivar;
+    }
+
+    public String getCommon() {
+        return common;
+    }
+
+    public void setCommon(String common) {
+        this.common = common;
+    }
+
+    public int getCacheId() {
+        return cacheId;
+    }
+
+    public void setCacheId(int cacheId) {
+        this.cacheId = cacheId;
+    }
+}
