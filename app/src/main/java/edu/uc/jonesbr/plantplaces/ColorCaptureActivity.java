c149188f0514a699597e722300fe7b1b3a1cb3bd
jonesbr <brandan.jones@uc.edu>
2018-06-06 21:27:08 -0400

    Fixed permissions to open an image from Gallery View

--- app/src/main/java/edu/uc/jonesbr/plantplaces/ColorCaptureActivity.java
+++ app/src/main/java/edu/uc/jonesbr/plantplaces/ColorCaptureActivity.java
@@ -1,12 +1,17 @@
 package edu.uc.jonesbr.plantplaces;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -28,6 +33,8 @@ public class ColorCaptureActivity extends PlantPlacesActivity {
 
     public static final int CAMERA_REQUEST_CODE = 1997;
     public static final int GALLERY_REQUEST_CODE = 1998;
+    public static final int READ_EXTERNAL_PERMISSION_REQUEST_CODE = 1999;
+
 
     @BindView(R.id.imgThumbnail)
     ImageView imgThumbnail;
@@ -65,15 +72,38 @@ public class ColorCaptureActivity extends PlantPlacesActivity {
         startActivityForResult(intent, CAMERA_REQUEST_CODE);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @OnClick(R.id.btnOpenGallery)
     public void btnOpenGalleryClicked() {
+        if(checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            // the user has already granted permission to us, so we can open the gallery.
+            invokeGallery();
+        } else {
+            // the user has not yet granted permission, or has revoked it, and we must request it.
+            String[] permissionRequest = {Manifest.permission.READ_EXTERNAL_STORAGE};
+            requestPermissions(permissionRequest, READ_EXTERNAL_PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == READ_EXTERNAL_PERMISSION_REQUEST_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                invokeGallery();
+            } else {
+                Toast.makeText(this, R.string.noExternalReadPermission, Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    private void invokeGallery() {
         Intent intent = new Intent(Intent.ACTION_PICK);
         File filePictureDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
         Uri data = Uri.parse(filePictureDirectory.getPath());
         String type = "image/*";
         intent.setDataAndType(data, type);
         startActivityForResult(intent, GALLERY_REQUEST_CODE);
-
     }
 
     @Override
